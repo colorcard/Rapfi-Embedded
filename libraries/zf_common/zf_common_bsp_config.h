8d548c9919be156f52e8812582f39b032c7c04a2
colorcard <liwq6688@icloud.com>
2026-09-13 20:52:11 +0800

    SPI 提高到 85MHz（/2），把整屏刷屏压到面板一帧内以减轻撕裂

--- libraries/zf_common/zf_common_bsp_config.h
+++ libraries/zf_common/zf_common_bsp_config.h
@@ -32,7 +32,7 @@
 #define I2C_FALL_TIME_NS        10U     /* TODO: 按上拉/总线电容确认。 */
 
 /* ------------------------------- SPI ---------------------------------- */
-#define SPI_DEFAULT_PRESCALER   SPI_BAUDRATEPRESCALER_4 /* /4 = 42.5MHz（PCLK2=170MHz），LCD 刷屏用。 */
+#define SPI_DEFAULT_PRESCALER   SPI_BAUDRATEPRESCALER_2 /* /2 = 85MHz（PCLK2=170MHz），缩短刷屏时间以减轻撕裂。 */
 #define SPI_DEFAULT_CPOL        SPI_POLARITY_LOW        /* TODO: 按器件确认。 */
 #define SPI_DEFAULT_CPHA        SPI_PHASE_1EDGE         /* TODO: 按器件确认。 */
 
